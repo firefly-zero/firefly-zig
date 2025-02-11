c3c939df3038d30a12878ed0235e1b916d9ce52b
gram <git@orsinium.dev>
2025-02-11 09:34:46 +0100

    Freq and Time constants and constructors

--- src/audio.zig
+++ src/audio.zig
@@ -1,7 +1,188 @@
 const bindings = @import("./bindings.zig").audio;
 
-pub const Freq = f32;
-pub const Time = u32;
+pub const sample_rate: u32 = 44_100;
+
+pub const Freq = struct {
+    h: f32,
+
+    pub const zero = Freq{ .h = 0 };
+    /// C0, MIDI note #12
+    pub const c0 = Freq{ .h = 16.351 };
+    pub const cs0 = Freq{ .h = 17.324 };
+    pub const d0 = Freq{ .h = 18.354 };
+    pub const ds0 = Freq{ .h = 19.445 };
+    pub const e0 = Freq{ .h = 20.601 };
+    pub const f0 = Freq{ .h = 21.827 };
+    pub const fs0 = Freq{ .h = 23.124 };
+    pub const g0 = Freq{ .h = 24.499 };
+    pub const gs0 = Freq{ .h = 25.956 };
+    /// A0, the lowest note of a piano
+    pub const a0 = Freq{ .h = 27.5 };
+    pub const as0 = Freq{ .h = 29.135 };
+    /// B0, the lowest note of a 5 string bass
+    pub const b0 = Freq{ .h = 30.868 };
+    /// C1, the lowest note of double bass with C extension
+    pub const c1 = Freq{ .h = 32.703 };
+    pub const cs1 = Freq{ .h = 34.648 };
+    pub const d1 = Freq{ .h = 36.708 };
+    pub const ds1 = Freq{ .h = 38.891 };
+    /// E1, the lowest note of a bass
+    pub const e1 = Freq{ .h = 41.203 };
+    pub const f1 = Freq{ .h = 43.654 };
+    pub const fs1 = Freq{ .h = 46.249 };
+    pub const g1 = Freq{ .h = 48.999 };
+    pub const gs1 = Freq{ .h = 51.913 };
+    pub const a1 = Freq{ .h = 55.0 };
+    pub const as1 = Freq{ .h = 58.27 };
+    pub const b1 = Freq{ .h = 61.735 };
+    pub const c2 = Freq{ .h = 65.406 };
+    pub const cs2 = Freq{ .h = 69.296 };
+    pub const d2 = Freq{ .h = 73.416 };
+    pub const ds2 = Freq{ .h = 77.782 };
+    /// E2, the lowest note of a guitar.
+    pub const e2 = Freq{ .h = 82.407 };
+    pub const f2 = Freq{ .h = 87.307 };
+    pub const fs2 = Freq{ .h = 92.499 };
+    pub const g2 = Freq{ .h = 97.999 };
+    pub const gs2 = Freq{ .h = 103.826 };
+    pub const a2 = Freq{ .h = 110.0 };
+    pub const as2 = Freq{ .h = 116.541 };
+    pub const b2 = Freq{ .h = 123.471 };
+    pub const c3 = Freq{ .h = 130.813 };
+    pub const cs3 = Freq{ .h = 138.591 };
+    pub const d3 = Freq{ .h = 146.832 };
+    pub const ds3 = Freq{ .h = 155.563 };
+    pub const e3 = Freq{ .h = 164.814 };
+    pub const f3 = Freq{ .h = 174.614 };
+    pub const fs3 = Freq{ .h = 184.997 };
+    /// G3, the lowest note of a violin.
+    pub const g3 = Freq{ .h = 195.998 };
+    pub const gs3 = Freq{ .h = 207.652 };
+    pub const a3 = Freq{ .h = 220.0 };
+    pub const as3 = Freq{ .h = 233.082 };
+    pub const b3 = Freq{ .h = 246.942 };
+    /// C4, the "middle C".
+    pub const c4 = Freq{ .h = 261.626 };
+    pub const cs4 = Freq{ .h = 277.183 };
+    pub const d4 = Freq{ .h = 293.665 };
+    pub const ds4 = Freq{ .h = 311.127 };
+    pub const e4 = Freq{ .h = 329.628 };
+    pub const f4 = Freq{ .h = 349.228 };
+    pub const fs4 = Freq{ .h = 369.994 };
+    pub const g4 = Freq{ .h = 391.995 };
+    pub const gs4 = Freq{ .h = 415.305 };
+    /// A4, the tuning reference note.
+    pub const a4 = Freq{ .h = 440.0 };
+    pub const as4 = Freq{ .h = 466.164 };
+    pub const b4 = Freq{ .h = 493.883 };
+    pub const c5 = Freq{ .h = 523.251 };
+    pub const cs5 = Freq{ .h = 554.365 };
+    pub const d5 = Freq{ .h = 587.33 };
+    pub const ds5 = Freq{ .h = 622.254 };
+    pub const e5 = Freq{ .h = 659.255 };
+    pub const f5 = Freq{ .h = 698.456 };
+    pub const fs5 = Freq{ .h = 739.989 };
+    pub const g5 = Freq{ .h = 783.991 };
+    pub const gs5 = Freq{ .h = 830.609 };
+    pub const a5 = Freq{ .h = 880.0 };
+    pub const as5 = Freq{ .h = 932.328 };
+    pub const b5 = Freq{ .h = 987.767 };
+    pub const c6 = Freq{ .h = 1046.502 };
+    pub const cs6 = Freq{ .h = 1108.731 };
+    pub const d6 = Freq{ .h = 1174.659 };
+    pub const ds6 = Freq{ .h = 1244.508 };
+    pub const e6 = Freq{ .h = 1318.51 };
+    pub const f6 = Freq{ .h = 1396.913 };
+    pub const fs6 = Freq{ .h = 1479.978 };
+    pub const g6 = Freq{ .h = 1567.982 };
+    pub const gs6 = Freq{ .h = 1661.219 };
+    pub const a6 = Freq{ .h = 1760.0 };
+    pub const as6 = Freq{ .h = 1864.655 };
+    pub const b6 = Freq{ .h = 1975.533 };
+    pub const c7 = Freq{ .h = 2093.005 };
+    pub const cs7 = Freq{ .h = 2217.461 };
+    pub const d7 = Freq{ .h = 2349.318 };
+    pub const ds7 = Freq{ .h = 2489.016 };
+    pub const e7 = Freq{ .h = 2637.021 };
+    pub const f7 = Freq{ .h = 2793.826 };
+    pub const fs7 = Freq{ .h = 2959.955 };
+    pub const g7 = Freq{ .h = 3135.964 };
+    pub const gs7 = Freq{ .h = 3322.438 };
+    pub const a7 = Freq{ .h = 3520.0 };
+    pub const as7 = Freq{ .h = 3729.31 };
+    pub const b7 = Freq{ .h = 3951.066 };
+    /// C8, the highest note of a piano.
+    pub const c8 = Freq{ .h = 4186.009 };
+    pub const cs8 = Freq{ .h = 4434.922 };
+    pub const d8 = Freq{ .h = 4698.636 };
+    pub const ds8 = Freq{ .h = 4978.032 };
+    pub const e8 = Freq{ .h = 5274.042 };
+    pub const f8 = Freq{ .h = 5587.652 };
+    pub const fs8 = Freq{ .h = 5919.91 };
+    pub const g8 = Freq{ .h = 6271.928 };
+    pub const gs8 = Freq{ .h = 6644.876 };
+    pub const a8 = Freq{ .h = 7040.0 };
+    pub const as8 = Freq{ .h = 7458.62 };
+    pub const b8 = Freq{ .h = 7902.132 };
+    pub const c9 = Freq{ .h = 8372.018 };
+    pub const cs9 = Freq{ .h = 8869.844 };
+    pub const d9 = Freq{ .h = 9397.272 };
+    pub const ds9 = Freq{ .h = 9956.064 };
+    pub const e9 = Freq{ .h = 10548.084 };
+    pub const f9 = Freq{ .h = 11175.304 };
+    pub const fs9 = Freq{ .h = 11839.82 };
+    pub const g9 = Freq{ .h = 12543.856 };
+    /// G#9, MIDI note #128, the top of the MIDI tuning range.
+    pub const gs9 = Freq{ .h = 13289.752 };
+    pub const a9 = Freq{ .h = 14080.0 };
+    pub const as9 = Freq{ .h = 14917.24 };
+    /// B9. For most of adults, it is already beyond the hearing range.
+    pub const b9 = Freq{ .h = 15804.264 };
+
+    pub fn hz(h: f32) Freq {
+        return Freq{ .h = h };
+    }
+
+    pub fn midi(note: u8) Freq {
+        // https://inspiredacoustics.com/en/MIDI_note_numbers_and_center_frequencies
+        // https://en.wikipedia.org/wiki/Musical_note#MIDI
+        var f: f32 = switch (note % 12) {
+            0 => 8.1758,
+            1 => 8.66,
+            2 => 9.18,
+            3 => 9.72,
+            4 => 10.30,
+            5 => 10.91,
+            6 => 11.56,
+            7 => 12.25,
+            8 => 12.98,
+            9 => 13.75,
+            10 => 14.57,
+            else => 15.43,
+        };
+        const oct = note / 12;
+        f *= @floatFromInt(1 << oct);
+        return Freq{ .h = f };
+    }
+};
+
+pub const Time = struct {
+    s: u32,
+
+    pub const zero = Time{ .s = 0 };
+
+    pub fn samples(s: u32) Time {
+        return Time{ .s = s };
+    }
+
+    pub fn seconds(s: u32) Time {
+        return Time{ .s = s * sample_rate };
+    }
+
+    pub fn ms(s: u32) Time {
+        return Time{ .s = s * sample_rate / 1000 };
+    }
+};
 
 fn Node(comptime T: type) type {
     return struct {
@@ -10,25 +191,25 @@ fn Node(comptime T: type) type {
 
         /// Add sine wave oscillator source (`∿`).
         pub fn addSine(self: BaseNode, f: Freq, phase: f32) Sine {
-            const id = bindings.add_sine(self.id, f, phase);
+            const id = bindings.add_sine(self.id, f.h, phase);
             return Sine{ .id = id };
         }
 
         /// Add square wave oscillator source (`⎍`).
         pub fn addSquare(self: BaseNode, f: Freq, phase: f32) Square {
-            const id = bindings.add_square(self.id, f, phase);
+            const id = bindings.add_square(self.id, f.h, phase);
             return Square{ .id = id };
         }
 
         /// Add sawtooth wave oscillator source (`╱│`).
         pub fn addSawtooth(self: BaseNode, f: Freq, phase: f32) Sawtooth {
-            const id = bindings.add_sawtooth(self.id, f, phase);
+            const id = bindings.add_sawtooth(self.id, f.h, phase);
             return Sawtooth{ .id = id };
         }
 
         /// Add triangle wave oscillator source (`╱╲`).
         pub fn addTriangle(self: BaseNode, f: Freq, phase: f32) Triangle {
-            const id = bindings.add_triangle(self.id, f, phase);
+            const id = bindings.add_triangle(self.id, f.h, phase);
             return Triangle{ .id = id };
         }
 
@@ -165,7 +346,7 @@ pub const Modulator = fn (node_id: u32, param: u32) void;
 pub fn LinearModulator(start: f32, end: f32, start_at: Time, end_at: Time) Modulator {
     return struct {
         fn mod(node_id: u32, param: u32) void {
-            bindings.mod_linear(node_id, param, start, end, start_at, end_at);
+            bindings.mod_linear(node_id, param, start, end, start_at.s, end_at.s);
         }
     }.mod;
 }
