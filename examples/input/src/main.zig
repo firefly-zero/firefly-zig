c981b8467da11512c4da57436881dcf89415787e
tranzystorekk <tranzystorek.io@protonmail.com>
2026-01-03 07:17:45 +0100

    example(input): show dpad

--- examples/input/src/main.zig
+++ examples/input/src/main.zig
@@ -11,6 +11,22 @@ const p_e = ff.Point{ .x = 190, .y = 90 };
 const p_w = ff.Point{ .x = 160, .y = 70 };
 const p_n = ff.Point{ .x = 190, .y = 60 };
 
+const u_a: ff.Point = .{ .x = 60, .y = 55 };
+const u_b: ff.Point = .{ .x = 80, .y = 55 };
+const u_c: ff.Point = .{ .x = 70, .y = 40 };
+
+const d_a: ff.Point = .{ .x = 60, .y = 85 };
+const d_b: ff.Point = .{ .x = 80, .y = 85 };
+const d_c: ff.Point = .{ .x = 70, .y = 100 };
+
+const l_a: ff.Point = .{ .x = 55, .y = 60 };
+const l_b: ff.Point = .{ .x = 55, .y = 80 };
+const l_c: ff.Point = .{ .x = 40, .y = 70 };
+
+const r_a: ff.Point = .{ .x = 85, .y = 60 };
+const r_b: ff.Point = .{ .x = 85, .y = 80 };
+const r_c: ff.Point = .{ .x = 100, .y = 70 };
+
 pub export fn render() void {
     drawBg();
     drawPad();
@@ -40,6 +56,24 @@ fn drawBg() void {
 fn drawPad() void {
     const maybePad = ff.readPad(ff.Peer.combined);
     if (maybePad) |pad| {
+        const dpad = pad.toDPad();
+        const t_style: ff.Style = .{
+            .fill_color = ff.Color.green,
+            .stroke_color = ff.Color.black,
+            .stroke_width = 3,
+        };
+        if (dpad.left) {
+            ff.drawTriangle(l_a, l_b, l_c, t_style);
+        }
+        if (dpad.right) {
+            ff.drawTriangle(r_a, r_b, r_c, t_style);
+        }
+        if (dpad.up) {
+            ff.drawTriangle(u_a, u_b, u_c, t_style);
+        }
+        if (dpad.down) {
+            ff.drawTriangle(d_a, d_b, d_c, t_style);
+        }
         const touch_pos = ff.Point{
             .x = pad_radius + @divTrunc(pad.x, 20),
             .y = pad_radius - @divTrunc(pad.y, 20),
