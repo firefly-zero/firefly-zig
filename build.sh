7dc78637b8908616113fb31f0509150deca6642b
gram <git@orsinium.dev>
2026-01-05 18:56:18 +0100

    try dev build of zig

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # Build script for static docs used on Cloudflare Pages.
 set -e
-wget https://ziglang.org/download/0.15.2/zig-x86_64-linux-0.15.2.tar.xz
+wget https://ziglang.org/builds/zig-x86_64-linux-0.16.0-dev.1912+0cbaaa5eb.tar.xz
 tar xf zig-*.tar.xz
 rm zig-*.tar.xz
 ./zig-*/zig build-lib -femit-docs src/root.zig
