eb65edc97b2d6b8eb925b91915113a353f799e89
gram <git@orsinium.dev>
2026-01-11 17:45:45 +0100

    build docs for wasm

--- build.sh
+++ build.sh
@@ -4,5 +4,5 @@ set -e
 wget https://ziglang.org/builds/zig-x86_64-linux-0.16.0-dev.1912+0cbaaa5eb.tar.xz
 tar xf zig-*.tar.xz
 rm zig-*.tar.xz
-./zig-*/zig build-lib -femit-docs src/root.zig
+./zig-*/zig build-lib -femit-docs src/root.zig -target wasm32-freestanding
 rm libroot.a
